f82bf4b2630bfaa0fc8e838285c95d9a407e06f9
SeanZhang-QED <66594541+SeanZhang-QED@users.noreply.github.com>
2022-08-24 18:55:09 -0700

    Finished the Login component.
    Implemented login to firebase, and login with google account, reset password through email

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,99 +1,218 @@
 import React, { useState } from 'react'
-import { CssBaseline, Grid, TextField, Button, Link, Box, Avatar, Typography, Divider, IconButton, InputAdornment } from '@mui/material'
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+    CssBaseline, Grid, TextField, Button, Box, Typography, Divider, IconButton, InputAdornment,
+    Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Link as MuiLink, Alert, AlertTitle
+} from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import GoogleIcon from '@mui/icons-material/Google';
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../contexts/AuthContext'
+
+function LoginWithGoogle() {
+    const [loading, setLoading] = useState(false)
+    const { loginWithGoogle } = useAuth()
+    const navigate = useNavigate()
+
+    async function handleLogin() {
+        try {
+            setLoading(true)
+            await loginWithGoogle()
+            navigate('/')
+            console.log("login success")
+        } catch (err) {
+            console.log(err.message)
+        }
+        setLoading(false)
+    }
+
+    return (
+        <Box sx={{ mt: 1 }} style={{ width: '100%' }}>
+            <Divider variant='middle'>
+                <Typography variant="body2" align="center" color='gray'>
+                    Or log in with
+                </Typography>
+            </Divider>
+            <Box style={{ width: '100%' }} display="flex" justifyContent="center" alignItems="center">
+                <IconButton onClick={handleLogin} disabled={loading} size='large'>
+                    <GoogleIcon color='primary' fontSize='large' />
+                </IconButton>
+            </Box>
+        </Box>
+    )
+}
+
+
+function ResetDialog(props) {
+    const { isReset, handleClose } = props;
+
+    function handleReset(event) {
+        event.preventDefault();
+        console.log(event.currentTarget)
+    }
+
+    return (
+        <Dialog open={isReset} onClose={handleClose}>
+            <DialogTitle>Reset Password</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    To reset your password, please enter your email address here.
+                    And check your inbox for futher instruction.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    variant="standard"
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleReset}>submit</Button>
+            </DialogActions>
+        </Dialog>
+    )
+
+}
+
 
 function LoginForm() {
     const [showPassword, setShowPassword] = useState(false);
+    const [error, setError] = useState('');
+    const [isPEmpty, setIsPEmpty] = useState(false);
+    const [isUEmpty, setIsUEmpty] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [isReset, setIsReset] = useState(false);
 
-    const handleSubmit = (event) => {
+    const { login } = useAuth()
+    const navigate = useNavigate()
+
+    async function handleSubmit(event) {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
             email: data.get('email'),
             password: data.get('password'),
         });
+
+        try {
+            setError('')
+            setLoading(true)
+            await login(data.get('email'), data.get('password'))
+            navigate('/')
+            console.log("login success")
+        } catch (err) {
+            setError(err.message)
+        }
+        setLoading(false)
     };
 
     return (
         <>
-        <CssBaseline />
-        <Box
-            sx={{
-                my: 8,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-
-            </Avatar>
-            <Typography variant="h5" gutterBottom marked="center" align="center" color='brand_red.main'
-                sx={{ fontWeight: 'bold', mt: 4 }}>
-                Hi, Welcome Back
-            </Typography>
-            <Divider variant='middle'>
-                <Typography variant="body2" align="center" color='gray'>
-                    {'Sign in with Email address as Username'}
+            <CssBaseline />
+            <Box
+                sx={{
+                    my: 8,
+                    mx: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h5" gutterBottom marked="center" align="center" color='brand_red.main'
+                    sx={{ fontWeight: 'bold', mt: 4 }}>
+                    Hi, Welcome Back
                 </Typography>
-            </Divider>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    autoComplete="current-password"
-                    InputProps={{ // <-- This is where the toggle button is added.
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                    onClick={() => setShowPassword(!showPassword)}
-                                    onMouseDown={() => setShowPassword(!showPassword)}
-                                >
-                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                </IconButton>
-                            </InputAdornment>
-                        )
-                    }}
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    color="brand_red"
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                >
-                    Sign In
-                </Button>
-                <Grid container>
-                    <Grid item xs>
-                        <Link href="#" variant="body2">
-                            Forgot password?
-                        </Link>
+                {error !== ''
+                    ?
+                    <Alert severity="error">
+                        <AlertTitle>Error</AlertTitle>
+                        {error}
+                    </Alert>
+                    :
+                    <Typography variant="body2" align="center" color='gray'>
+                        Sign in with Email address
+                    </Typography>
+                }
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        error={isUEmpty}
+                        helperText={isUEmpty ? 'Email is required' : ''}
+                        onChange={(e) => {
+                            if (!e.target.value) {
+                                setIsUEmpty(true);
+                            } else {
+                                setIsUEmpty(false);
+                            }
+                        }}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type={showPassword ? "text" : "password"}
+                        id="password"
+                        autoComplete="current-password"
+                        InputProps={{ // <-- This is where the toggle button is added.
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        onMouseDown={() => setShowPassword(!showPassword)}
+                                    >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                        error={isPEmpty}
+                        helperText={isPEmpty ? 'Password is required' : ''}
+                        onChange={(e) => {
+                            if (!e.target.value) {
+                                setIsPEmpty(true);
+                            } else {
+                                setIsPEmpty(false);
+                            }
+                        }}
+                    />
+                    <Grid container style={{ marginTop: '15px' }}>
+                        <Grid item xs>
+                            <MuiLink onClick={() => { setIsReset(true) }} >
+                                Forget password?
+                            </MuiLink>
+                        </Grid>
+                        <Grid item>
+                            <Link to="/signup">Don't have an account? Sign up</Link>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Link href="#" variant="body2">
-                            {"Don't have an account? Sign Up"}
-                        </Link>
-                    </Grid>
-                </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        color="brand_red"
+                        variant="contained"
+                        disabled={loading}
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        <Typography color='white' fontWeight='bold'>
+                            {loading ? 'In progress' : 'Log In'}
+                        </Typography>
+                    </Button>
+                </Box>
+                <ResetDialog isReset={isReset} handleClose={() => { setIsReset(false) }} />
+                <LoginWithGoogle />
             </Box>
-        </Box>
         </>
     )
 }
@@ -115,7 +234,7 @@ export default function LoginPage() {
                     backgroundPosition: 'center',
                 }}
             />
-            <Grid item xs={12} sm={8} md={5} elevation={6} square>
+            <Grid item xs={12} sm={8} md={5} elevation={6}>
                 <LoginForm />
             </Grid>
         </Grid>
